d4527b600f2885b897f55f0cba3508eb494903fb
Ireaneus <bwdave@protonmail.com>
2022-05-01 16:46:11 -0600

    Cool setup a login / Register screen With expressions for jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-function Form() {
+function Form(props) {
   return (
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      {!props.isRegistered && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
     </form>
   );
 }
 
 export default Form;
+// props.isRegisterd === false is an alt to the given
+// && is the same as : null
